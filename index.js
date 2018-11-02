7a74d5aab6f0a216cfaed7edb764b9c185e8afbc
b3x0b <b3x0b@ugrad.cs.ubc.ca>
2018-11-02 13:38:59 -0700

    Made it work with nested directories

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // import * as fs from "fs-extra";
 const fs = require("fs-extra");
+const recursive = require("recursive-readdir");
 
-
-fs.readdir("queries/").then(async (queryNames) => {
+recursive("queries/").then(async (queryNames) => {
     let testFile = {};
     let count = 0;
     for (let queryName of queryNames) {
-        let file = await fs.readFile("queries/" + queryName);
+        let file = await fs.readFile(queryName);
         let fileContents = JSON.parse(file);
         let content = fileContents.query;
         testFile["query" + count] = content;
